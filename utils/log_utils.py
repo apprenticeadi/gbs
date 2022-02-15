5953ab7af0ebc0f384bb18f012ac40f48e16c272
apprenticeadi <zhenghao.li.1999@gmail.com>
2022-02-15 11:06:48 +0000

    add log utils on linux

--- utils/log_utils.py
+++ utils/log_utils.py
@@ -6,6 +6,20 @@ import os
 class LogUtils:
 
     @staticmethod
+    def log_config_lin():
+        time_stamp = datetime.datetime.now().strftime("%d-%b-%Y-(%H.%M.%S.%f)")
+        logging_filename = r'../Results/logs/{}.txt'.format(time_stamp)
+
+        os.makedirs(os.path.dirname(logging_filename), exist_ok=True)
+        stdout_handler = logging.StreamHandler(sys.stdout)
+
+        logging.basicConfig(filename=logging_filename, level=logging.DEBUG,
+                            format='%(levelname)s %(asctime)s %(message)s')
+
+        # make logger print to console (it will not if multithreaded)
+        logging.getLogger().addHandler(stdout_handler)
+
+    @staticmethod
     def log_config():
         time_stamp = datetime.datetime.now().strftime("%d-%b-%Y-(%H.%M.%S.%f)")
         logging_filename = r'..\Results\logs\{}.txt'.format(time_stamp)
