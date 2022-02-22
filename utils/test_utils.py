562d9ef0e004d8981f34f539be15347e1c0e2373
apprenticeadi <zhenghao.li.1999@gmailcom>
2022-02-22 17:42:34 +0000

    add function that gives all possible n with fixed N sum

--- utils/test_utils.py
+++ utils/test_utils.py
@@ -12,4 +12,13 @@ class TestUtils:
 
         return n_list
 
+    @staticmethod
+    def all_possible_n_fix_sum(N_cutoff, N_sum, M):
+        n_list = []
+        for n_photon in product(range(N_cutoff+1), repeat=M):
+            if sum(n_photon) == N_sum:
+                n_list.append(list(n_photon))
+
+        return n_list
+
 
