db418fcc446424d521e111908d394e250c50d86b
apprenticeadi <zhenghao.li.1999@gmailcom>
2022-02-22 16:59:32 +0000

    Add utils function that gives all possible photon pattern given cutoff N, max photon number and number of modes

--- utils/test_utils.py
+++ utils/test_utils.py
@@ -1,2 +1,15 @@
+from itertools import product
+
+class TestUtils:
+
+    @staticmethod
+    def all_possible_n(N_cutoff, N_sum_max, M):
+
+        n_list = []
+        for n_photon in product(range(N_cutoff+1), repeat=M):
+            if sum(n_photon) <= N_sum_max:
+                n_list.append(list(n_photon))
+
+        return n_list
 
 
